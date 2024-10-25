6635684589c0a1a2236cae08d729806ef0dbe4a1
HIENLTH 2023 <hyhien@gmail.com>
2024-10-25 12:07:04 +0700

    Add to cart

---
+++ MyEStore/MyEStore/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEStore.Controllers
+{
+	public class CartController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult AddToCart(int id)
+		{
+			return View();
+		}
+	}
+}
