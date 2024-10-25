755f98341009caa28b135788dccfd7c19d8c66b3
HIENLTH 2023 <hyhien@gmail.com>
2024-10-25 11:39:58 +0700

    Init

---
+++ MyEStore/MyEStore/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEStore.Entities;
+using MyEStore.Models;
+
+namespace MyEStore.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly MyeStoreContext _ctx;
+
+		public ProductsController(MyeStoreContext ctx) {
+			_ctx = ctx;
+		}
+
+		public IActionResult Index()
+		{
+			var data = _ctx.HangHoas.AsQueryable();
+
+			var result = data.Select(hh => new HangHoaVM
+			{
+				MaHh = hh.MaHh, TenHh = hh.TenHh,
+				DonGia = hh.DonGia ?? 0, Hinh = hh.Hinh
+			});
+			return View(result);
+		}
+	}
+}
