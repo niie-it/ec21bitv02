a5654539227eb44d94998b029ab7feb5e355afe5
HIENLTH 2023 <hyhien@gmail.com>
2024-10-26 10:59:21 +0700

    Menu Loai

--- MyEStore/MyEStore/Controllers/ProductsController.cs
+++ MyEStore/MyEStore/Controllers/ProductsController.cs
@@ -4,24 +4,31 @@ using MyEStore.Models;
 
 namespace MyEStore.Controllers
 {
-	public class ProductsController : Controller
-	{
-		private readonly MyeStoreContext _ctx;
+    public class ProductsController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
 
-		public ProductsController(MyeStoreContext ctx) {
-			_ctx = ctx;
-		}
+        public ProductsController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
 
-		public IActionResult Index()
-		{
-			var data = _ctx.HangHoas.AsQueryable();
+        public IActionResult Index(int? cateid)
+        {
+            var data = _ctx.HangHoas.AsQueryable();
+            if (cateid.HasValue)
+            {
+                data = data.Where(hh => hh.MaLoai == cateid.Value);
+            }
 
-			var result = data.Select(hh => new HangHoaVM
-			{
-				MaHh = hh.MaHh, TenHh = hh.TenHh,
-				DonGia = hh.DonGia ?? 0, Hinh = hh.Hinh
-			});
-			return View(result);
-		}
-	}
+            var result = data.Select(hh => new HangHoaVM
+            {
+                MaHh = hh.MaHh,
+                TenHh = hh.TenHh,
+                DonGia = hh.DonGia ?? 0,
+                Hinh = hh.Hinh
+            });
+            return View(result);
+        }
+    }
 }
---
+++ MyEStore/MyEStore/ViewComponents/LoaiViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEStore.Entities;
+
+namespace MyEStore.ViewComponents
+{
+    public class LoaiViewComponent : ViewComponent
+    {
+        private readonly MyeStoreContext _ctx;
+
+        public LoaiViewComponent(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_ctx.Loais.ToList());
+        }
+    }
+}
