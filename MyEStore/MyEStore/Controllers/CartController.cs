1af2e2833134e9dc74b6fb1f728d5be5b5450922
HIENLTH 2023 <hyhien@gmail.com>
2024-11-01 10:29:13 +0700

    Add to cart and Show cart

--- MyEStore/MyEStore/Controllers/CartController.cs
+++ MyEStore/MyEStore/Controllers/CartController.cs
@@ -4,37 +4,63 @@ using MyEStore.Models;
 
 namespace MyEStore.Controllers
 {
-    public class CartController : Controller
-    {
-        const string CART_KEY = "MY_CART";
-        private readonly MyeStoreContext _ctx;
+	public class CartController : Controller
+	{
+		const string CART_KEY = "MY_CART";
+		private readonly MyeStoreContext _ctx;
 
-        public List<CartItem> CartItems
-        {
-            get
-            {
-                var carts = HttpContext.Session.Get<List<CartItem>>(CART_KEY);
-                if (carts == null)
-                {
-                    carts = new List<CartItem>();
-                }
-                return carts;
-            }
-        }
+		public List<CartItem> CartItems
+		{
+			get
+			{
+				var carts = HttpContext.Session.Get<List<CartItem>>(CART_KEY);
+				if (carts == null)
+				{
+					carts = new List<CartItem>();
+				}
+				return carts;
+			}
+		}
 
-        public CartController(MyeStoreContext ctx)
-        {
-            _ctx = ctx;
-        }
+		public CartController(MyeStoreContext ctx)
+		{
+			_ctx = ctx;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View(CartItems);
+		}
 
-        public IActionResult AddToCart(int id)
-        {
-            return View();
-        }
-    }
+		public IActionResult AddToCart(int id, int qty = 1)
+		{
+			var cart = CartItems;
+			var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
+			if (cartItem != null)
+			{
+				cartItem.SoLuong += qty;
+			}
+			else
+			{
+				var hangHoa = _ctx.HangHoas.SingleOrDefault(h => h.MaHh == id);
+				if (hangHoa == null)
+				{
+					//không có trong database
+					TempData["ThongBao"] = $"Không tìm thấy hàng hóa có mã {id}";
+					return RedirectToAction("Index", "Products");
+				}
+				cartItem = new CartItem
+				{
+					MaHh = id,
+					SoLuong = qty,
+					TenHh = hangHoa.TenHh,
+					Hinh = hangHoa.Hinh,
+					DonGia = hangHoa.DonGia ?? 0
+				};
+				cart.Add(cartItem);
+			}
+			HttpContext.Session.Set(CART_KEY, cart);
+			return RedirectToAction("Index");
+		}
+	}
 }
